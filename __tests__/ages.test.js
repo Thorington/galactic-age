4880bb56c13bf8f9d47f813dd9afad411f8aae15
Claire Thorington <claire.thorington@gmail.com>
2022-06-24 09:15:21 -0700

    write first test: age object

---
+++ __tests__/ages.test.js
@@ -0,0 +1,10 @@
+import Age from './../src/age.js';
+
+describe('Age', () => {
+
+  test('should correctly create an age object with inputted Earth age', () => {
+    const earthAge = new Age(25);
+    expect(earthAge.age).toEqual(25);
+  })
+
+});
