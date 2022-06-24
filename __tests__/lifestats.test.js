dcbab2ff8da04cf30284553ecc41b75b9912250f
Claire Thorington <claire.thorington@gmail.com>
2022-06-24 10:58:24 -0700

    write second and third tests: return remaining Earth years from inputted Earth age and life expectancy, return remaining Mercury years for same

--- __tests__/lifestats.test.js
+++ __tests__/lifestats.test.js
@@ -3,15 +3,20 @@ import LifeStats from '../src/lifestats.js';
 describe('LifeStats', () => {
 
   test('should correctly create life-stats object from inputted Earth age and life expectancy', () => {
-    const statsOnEarth = new LifeStats(25,75);
+    const statsOnEarth = new LifeStats(25, 75);
     expect(statsOnEarth.age).toEqual(25);
     expect(statsOnEarth.lifespan).toEqual(75);
   })
 
-test('should correctly determine remaining Earth years from inputted Earth age and life expectancy', () => {
-    const remainingEarthYears = new LifeStats(25,75);
+  test('should correctly determine remaining Earth years from inputted Earth age and life expectancy', () => {
+    const remainingEarthYears = new LifeStats(25, 75);
     expect(remainingEarthYears.lastOnEarth()).toEqual(50);
   })
-  
+
+  test('should correctly determine remaining Mercury years from inputted Earth age and life expectancy', () => {
+    const remainingMercuryYears = new LifeStats(25, 75);
+    expect(remainingEarthYears.lastOnMercury()).toEqual(50);
   })
+
+})
 
