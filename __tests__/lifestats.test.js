4ab6d489bfe38f4426ffdfe8ccd40046b7a8a043
Claire Thorington <claire.thorington@gmail.com>
2022-06-24 11:48:29 -0700

    write fourth test: return remaining Mars years from inputted Earth age and lifespan

--- __tests__/lifestats.test.js
+++ __tests__/lifestats.test.js
@@ -23,5 +23,10 @@ describe('LifeStats', () => {
     expect(earthYears.lastOnVenus()).toEqual(31);
   })
 
+  test('should correctly determine remaining Mars years from inputted Earth age and life expectancy', () => {
+    const earthYears = new LifeStats(25, 75);
+    expect(earthYears.lastOnMars()).toEqual(31);
+  })
+
 })
 
