2bc6fd6c9221ee20e010440e8eb4a785f8209538
Claire Thorington <claire.thorington@gmail.com>
2022-06-24 14:56:43 -0700

    oh my god! Just spent an hour trying to get math to match! It was returning 3.99999998 when it should've been returning 4! Aren't computers supposed to be good at math? Added a .toFixed(1) to round to the nearest tenth.

--- src/lifestats.js
+++ src/lifestats.js
@@ -5,30 +5,34 @@ export default class LifeStats {
     this.lifespan = lifespan;
   }
   lastOnEarth() {
-      if (this.age > this.lifespan) {
-        let yearsPast = `You are ${this.age} years old on Earth. You've lived ${this.age-this.lifespan} Earth years past your life expectancy!`;
-        return (yearsPast);
-      } else {
-        let yearsLeft = `You are ${this.age} years old on Earth. You have approximately ${this.lifespan-this.age} Earth years left, if all goes well!`;
-      return (yearsLeft);
-      }
-      }
-    
-
-  lastOnMercury() {
-    const result = ((this.lifespan-this.age)*0.24);
-    return (result);
+    if (this.age > this.lifespan) {
+      let earthResult = `You are ${this.age} years old on Earth. You've lived ${this.age - this.lifespan} Earth years past your life expectancy!`;
+      return (earthResult);
+    } else {
+      let earthResult = `You are ${this.age} years old on Earth. You have approximately ${this.lifespan - this.age} Earth years left, if all goes well!`;
+      return (earthResult);
+    }
   }
+  lastOnMercury() {
+    if (this.age > this.lifespan) {
+      let mercuryResult = `You are ${(this.age * 0.24)} years old on Mercury. You've lived ${((this.age * 0.24) - (this.lifespan * 0.24)).toFixed(1)} Mercury years past your life expectancy!`;
+      return (mercuryResult);
+    } else {
+      let mercuryResult = `You are ${(this.age * 0.24)} years old on Mercury. You have approximately ${((this.lifespan * 0.24) - (this.age * 0.24))} Mercury years left, if all goes well!`;
+      return (mercuryResult);
+    }
+  }
+
   lastOnVenus() {
-    const result = ((this.lifespan-this.age)*0.62);
+    const result = ((this.lifespan - this.age) * 0.62);
     return (result);
   }
   lastOnMars() {
-    const result = ((this.lifespan-this.age)*1.88);
+    const result = ((this.lifespan - this.age) * 1.88);
     return (result);
   }
   lastOnJupiter() {
-    const result = ((this.lifespan-this.age)*11.86);
+    const result = ((this.lifespan - this.age) * 11.86);
     return (result);
   }
 }
