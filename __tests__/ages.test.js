b6738e57dc1ef0bae77cea100e5e37eb638a2018
Claire Thorington <claire.thorington@gmail.com>
2022-06-24 09:35:25 -0700

    write second test: Mercurify age

--- __tests__/ages.test.js
+++ __tests__/ages.test.js
@@ -7,4 +7,9 @@ describe('Age', () => {
     expect(earthAge.age).toEqual(25);
   })
 
+  test('should correctly determine age in Mercury years given age in Earth years', () => {
+    const earthAge = new Age(25);
+    expect(earthAge.Mercurify).toEqual(25);
+  })
+
 });
